f4c2a067ef34d7afb4e9f5de4712abbca62852b0
dvlpr2003 <gayathrigaya698@gmail.com>
2024-03-09 15:31:52 +0530

    viewset

--- cart/views.py
+++ cart/views.py
@@ -4,13 +4,15 @@ from .serializers import StudentSerializer
 from rest_framework.decorators import api_view
 from .models import *
 
-from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class GetList(ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields =["department","age"]
 
 
 
